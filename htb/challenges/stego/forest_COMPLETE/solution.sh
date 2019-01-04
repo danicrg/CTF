e2d4b75aa16b02a2a2dcfcf86dca9315ce6fc0d6
root <root@localhost.localdomain>
2019-01-04 04:23:30 -0500

    raining

---
+++ htb/challenges/stego/forest_COMPLETE/solution.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+steghide extract -sf forest.jpg -p IsJuS1Af0r3sTbR0;
+cat nothinghere.txt | tr 'A-Za-z' 'N-ZA-Mn-za-m'
---
+++ htb/challenges/stego/raining_blood_COMPLETE/solution.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+echo "SFRCe2gxZGQxbmdfZDR0NF9iM3R3MzNuX21wM19mcjRtM3NfaXNfbm90X2Z1bm55ISF9Cg==" | base64 -d
